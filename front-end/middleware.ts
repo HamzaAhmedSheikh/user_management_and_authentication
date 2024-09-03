8925699cc44219951082175d45898259a281de2d
Syed Kamil Zafar <136997351+kamilzafar@users.noreply.github.com>
2024-09-03 22:55:43 +0500

    chore: Remove unused auth.config.ts file and update middleware.ts

--- front-end/middleware.ts
+++ front-end/middleware.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { auth } from "./src/auth";
@@ -22,43 +21,3 @@ export async function middleware(req: NextRequest) {
   // Allow access to non-protected routes
   return NextResponse.next();
 }
-=======
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
-
-export default NextAuth(authConfig).auth;
-
-export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-//   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-    matcher: ["/dashboard"], // Define protected routes
-};
-// // middleware.ts
-// import { getToken } from "next-auth/jwt";
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-
-// export async function middleware(req: NextRequest) {
-//   // Define protected routes
-//   const protectedRoutes = ["/dashboard"];
-
-//   // Check if the current route is protected
-//   const isProtectedRoute = protectedRoutes.some(route => req.nextUrl.pathname.startsWith(route));
-
-//   // If it's a protected route, check for the session
-//   if (isProtectedRoute) {
-//     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-//     if (!token) {
-//       // If no token, redirect to sign-in page
-//       return NextResponse.redirect(new URL("/signin", req.url));
-//     }
-//   }
-
-//   // Allow access to non-protected routes
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ["/dashboard"], // Define protected routes
-// };
->>>>>>> 9224fd5b86eff6e9a8302b71b0e048c161ac0888
